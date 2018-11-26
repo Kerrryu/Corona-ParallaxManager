fa217d3867e7a0f50c369c92ff2398c6a3a7c57d
Sebastian Maj <WolfTech@cogeco.ca>
2018-11-25 22:20:45 -0500

    Added Author note

--- ParallaxManagerHorizontal.lua
+++ ParallaxManagerHorizontal.lua
@@ -1,5 +1,8 @@
 --[[
 
+Title: Horizontal Parallax Manager
+Author: Sebastian Maj
+
 Description:
 A Universal Parallax Module that can be used across many games
 handles all the Parallax work for you. Only Horizontal.
